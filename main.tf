e4f369e22366c146fe7e615d42473cd79921e18a
mridu-paban <129522397+mridu-paban@users.noreply.github.com>
2026-02-02 12:15:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
     random = {
-      source  = "hashicorp/random"
+      source = "hashicorp/random"
     }
   }
 }
@@ -17,53 +17,63 @@ resource "random_id" "rand_id" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "mywebapp-bucket" {
-  bucket = "mywebapp-bucket-${random_id.rand_id.hex}"
+resource "aws_s3_account_public_access_block" "account" {
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.mywebapp-bucket.id
+resource "aws_s3_bucket" "mywebapp_bucket" {
+  bucket        = "mywebapp-bucket-${random_id.rand_id.hex}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.mywebapp_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-}
 
-resource "aws_s3_bucket_policy" "mywebapp" {
-  bucket = aws_s3_bucket.mywebapp-bucket.id
-  policy = jsonencode(
-    {
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Sid       = "PublicReadGetObject",
-          Effect    = "Allow",
-          Principal = "*",
-          Action    = "s3:GetObject",
-          Resource  = "${aws_s3_bucket.mywebapp-bucket.arn}/*"
-        }
-      ]
-    }
-  )
+  depends_on = [aws_s3_account_public_access_block.account]
 }
 
 resource "aws_s3_bucket_website_configuration" "mywebapp" {
-  bucket = aws_s3_bucket.mywebapp-bucket.id
+  bucket = aws_s3_bucket.mywebapp_bucket.id
 
   index_document {
     suffix = "index.html"
   }
 }
 
-
 resource "aws_s3_object" "index_html" {
-  bucket       = aws_s3_bucket.mywebapp-bucket.bucket
+  bucket       = aws_s3_bucket.mywebapp_bucket.bucket
   source       = "./index.html"
   key          = "index.html"
   content_type = "text/html"
 }
 
-output "name" {
+resource "aws_s3_bucket_policy" "mywebapp" {
+  bucket = aws_s3_bucket.mywebapp_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.mywebapp_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.bucket]
+}
+
+output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.mywebapp.website_endpoint
 }
